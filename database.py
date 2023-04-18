6ab36b807b8463df5cfc8c395d10f75d5ea59349
Abhijeet kumar <abhijeet.kumar@sacumen.com>
2023-04-18 22:10:33 +0530

    added db connection and all other endpoint functions

---
+++ database.py
@@ -0,0 +1,41 @@
+from model import Todo
+
+#this is the mongoDB driver
+import motor.motor_asyncio as mongo_db_driver
+
+uri = ""
+
+client = mongo_db_driver.AsyncIOMotorClient(uri)
+
+database = client.TodoList
+collection = database.todo
+
+
+async def fetch_one_todo(title):
+    document = await collection.find_one({"title": title})
+    return document
+
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find({})
+    async for document in cursor:
+        todos.append(Todo(**document))
+        
+    return todos
+
+async def create_todo_data(todo: Todo):
+    document = todo
+    result = await collection.insert_one(document)
+    return document
+
+async def update_todo_data(title: str, description: str):
+    query = {"title": title}
+    newvalues = {"$set": {"description": description}}
+    await collection.update_one(query, newvalues)
+    document = collection.find_one(query)
+    return document
+
+async def delete_todo_data(title: str):
+    await collection.delete_one({"title": title})
+    return {"message": "Todo with title: {} deleted successfully!".format(title)}
+    
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
+from database import (create_todo_data, delete_todo_data, fetch_all_todos,
+                      fetch_one_todo, update_todo_data)
+from model import Todo
 
 app = FastAPI()
 
@@ -17,21 +21,29 @@ def read_root():
 
 @app.get('/api/todo')
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
-@app.get('/api/todo/{id}')
-async def get_todo_by_id(id: int):
-    return id
+@app.get('/api/todo/{title}', response_model=Todo)
+async def get_todo_by_title(title: int):
+    response = await fetch_one_todo(title)
+    return response
 
 @app.post('/api/todo')
-async def create_todo(todo):
-    return todo
+async def create_todo(todo: Todo):
+    response = await create_todo_data(todo.dict())
+    if response:
+        return response
+    
+    raise HTTPException(status_code=400, detail='Todo already exists')
 
-@app.update('/api/todo/{id}')
-async def update_todo_by_id(id: int, data):
-    return data
+@app.put('/api/todo/{title}')
+async def update_todo_by_title(title: int, data):
+    response = await update_todo_data(title, data)
+    return response
 
-@app.delete('/api/todo/{id}')
-async def delete_todo_by_id(id: int):
-    return id
+@app.delete('/api/todo/{title}')
+async def delete_todo_by_title(title: int):
+    response = await delete_todo_data(title)
+    return response
     
